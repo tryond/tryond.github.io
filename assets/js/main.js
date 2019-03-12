af8062fc5814a178242a22c5703473f76fc9eb11
Danny Tryon <tryon.dan@gmail.com>
2019-03-12 11:08:59 -0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+;(function(){
+
+			// Menu settings
+			$('#menuToggle, .menu-close').on('click', function(){
+				$('#menuToggle').toggleClass('active');
+				$('body').toggleClass('body-push-toleft');
+				$('#theMenu').toggleClass('menu-open');
+			});
+
+
+})(jQuery)
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
